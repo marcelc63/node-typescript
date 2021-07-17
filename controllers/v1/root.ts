19552afcf75acce2f745470d249896a05c2f79c1
Marcel <marcelc6363@gmail.com>
2021-07-17 15:36:07 +0700

    Update gitignore and root.ts format

--- controllers/v1/root.ts
+++ controllers/v1/root.ts
@@ -2,7 +2,6 @@ import KoaRouter from "koa-router";
 
 export const router = new KoaRouter();
 
-router
-  .get("/", async (context) => {
-    context.response.body = "Running!";
-  });
+router.get("/", async (context) => {
+  context.response.body = "Running!";
+});
