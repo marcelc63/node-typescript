4051ab4c4adc5c54ec7516253e86745bf71b7ad1
Marcel <marcelc6363@gmail.com>
2021-07-31 23:43:34 +0700

    Abstract away database configurations to config.js

--- config.ts
+++ config.ts
@@ -1,3 +1,8 @@
 export const APP_HOST = process.env.APP_HOST || "127.0.0.1";
 export const APP_PORT = process.env.APP_PORT || 8000;
 export const APP_ENV = process.env.APP_ENV || "local";
+export const DB_HOST = process.env.DB_HOST || "127.0.0.1";
+export const DB_USER = process.env.DB_USER || "root";
+export const DB_PASS = process.env.DB_PASS || "";
+export const DB_DATABASE = process.env.DB_DATABASE || "node-typescript";
+export const DB_PORT = process.env.DB_PORT || 3306;
--- server/database.ts
+++ server/database.ts
@@ -1,9 +1,13 @@
+import { DB_DATABASE, DB_HOST, DB_PASS, DB_USER, APP_ENV } from "~/config";
 import { Sequelize } from "sequelize";
 
 export const db: { sequelize: Sequelize } = {
-  sequelize: new Sequelize("datapinter_tracker", "root", "", {
-    host: "localhost",
+  sequelize: new Sequelize(DB_DATABASE, DB_USER, DB_PASS, {
+    host: DB_HOST,
     dialect: "mysql",
+    logging: ((): boolean => {
+      return APP_ENV === "local";
+    })(),
   }),
 };
 
