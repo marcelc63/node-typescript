30e1e44ec24dda8efeb0988b283c4b793902682f
Marcel <marcelc6363@gmail.com>
2021-10-23 16:05:00 +0700

    Add configs

--- config.ts
+++ config.ts
@@ -6,3 +6,11 @@ export const DB_USER = process.env.DB_USER || "root";
 export const DB_PASS = process.env.DB_PASS || "";
 export const DB_DATABASE = process.env.DB_DATABASE || "node-typescript";
 export const DB_PORT = process.env.DB_PORT || 3306;
+export const JWT_SECRET = process.env.JWT_SECRET || "i-love-iu";
+export const BASE_FRONTEND_URL =
+  process.env.BASE_FRONTEND_URL || "http://localhost:3000";
+export const CLOUDINARY_CLOUD_NAME =
+  process.env.CLOUDINARY_CLOUD_NAME || "test";
+export const CLOUDINARY_API_KEY = process.env.CLOUDINARY_API_KEY || "test";
+export const CLOUDINARY_API_SECRET =
+  process.env.CLOUDINARY_API_SECRET || "test";
